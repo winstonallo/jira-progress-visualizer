77f6e8e580d3147a1a7b696357f3e29a9f42a3e2
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-02-21 12:22:52 +0000

    add: check for 'Stream' issue type to make sure it is always on the bottom

--- visualizer.py
+++ visualizer.py
@@ -37,7 +37,9 @@ class GanttChart:
         self.df[self.start_date_field] = pd.to_datetime(self.df[self.start_date_field], errors='coerce').dt.date
         self.df[self.end_date_field] = pd.to_datetime(self.df[self.end_date_field], errors='coerce').dt.date
         self.df.dropna(subset=[self.end_date_field, self.start_date_field], inplace=True)
-        self.df.sort_values(self.start_date_field, inplace=True, ascending=False)
+        self.df['is_stream'] = self.df['Issue Type'].apply(lambda x: 1 if x == 'Stream' else 0)
+        self.df.sort_values(by=['is_stream', self.start_date_field], inplace=True, ascending=False)
+        self.df.drop('is_stream', axis=1, inplace=True)
 
     def generate_gantt_chart(self, output_path : str):
         fig, ax = plt.subplots(figsize=(10, 5))
