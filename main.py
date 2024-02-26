2d911d74cc5e8e4f78aa1015499267639d3a690d
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-02-26 10:59:58 +0000

    add: class for simple pdf list generation

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import os
 import glob
 import sys
-from visualizer.visualizer import GanttChart
+from visualizer.gantt_chart import GanttChart
 from visualizer.config import Config
+from visualizer.pdf import PDF
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         config_path = sys.argv[1]
     else:
         config_path = None
-    config_0 = Config('config_0.json').config
-    config_1 = Config('config_1.json').config
-    config_1_1 = Config('config_1.1.json').config
-    config_2 = Config('config_2.json').config
-    if not os.path.exists(config_1['directories']['target']):
-        os.makedirs(config_1['directories']['target'])
-    files = glob.glob(f"{config_1['directories']['csv']}/*.csv")
+    config = Config()
+    if not os.path.exists('diagrams'):
+        os.makedirs('diagrams')
+    files = glob.glob(f"data/*.csv")
     for file in files:
         print(file)
         if file[:7] == 'data/0_':
-            chart = GanttChart(file, config_0)
+            chart = GanttChart(file, config.get('0'))
         elif file[:7] == 'data/1_':
-            chart = GanttChart(file, config_1)
+            chart = GanttChart(file, config.get('1'))
         elif file[:9] == 'data/1.1_':
-            chart = GanttChart(file, config_1_1)
+            chart = GanttChart(file, config.get('1.1'))
         elif file[:7] == 'data/2_':
-            chart = GanttChart(file, config_2)
+            chart = GanttChart(file, config.get('2'))
+        elif file[:7] == 'data/3_':
+            chart = GanttChart(file, config.get('3'))
         chart.load_data()
         chart.save_plot()
         
--- visualizer/config.py
+++ visualizer/config.py
@@ -4,19 +4,25 @@ from visualizer.error import Error
 
 class Config:
 
-    def __init__(self, config_path : str = 'config.json'):
-        self.config_path = config_path
-        self.config = self.load_config()
+    def __init__(self):
+        self.configs = {
+            '0': self.load_config('config_0.json'),
+            '1': self.load_config('config_1.json'),
+            '1.1': self.load_config('config_1.1.json'),
+            '2': self.load_config('config_2.json'),
+            '3': self.load_config('config_3.json')
+        }
 
-    def load_config(self) -> dict[str, str]:
-        if os.path.exists(self.config_path) and os.path.getsize(self.config_path) > 0:
+    def load_config(self, path : str) -> dict[str, str]:
+        if os.path.exists(path) and os.path.getsize(path) > 0:
             try:
-                with open(self.config_path, 'r') as config_file:
+                with open(path, 'r') as config_file:
                     return json.load(config_file)
             except json.JSONDecodeError:
                 print('please provide a valid .json config file')
         else:
             Error('no config file found - aborting')
     
-    def get_config(self) ->dict[str, str]:
-        return self.config
+    def get(self, index : str) ->dict[str, str]:
+        return self.configs[index]
+    
